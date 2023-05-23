263a6118f127fe5525cc46fab57cd5406815aae3
Korbinian Maag <korbinian.maag@gmail.com>
2023-05-23 14:43:11 +0200

    Changed build path and adjusted src references so it works hopefully

--- src/js/path.js
+++ src/js/path.js
@@ -1,6 +1,6 @@
 // path.js
 const baseUrl = import.meta.env.MODE === 'development'
   ? '' // Empty string for development environment
-  : 'MidiToys'; // Replace <repo-name> with the repository name
+  : 'https://github.com/ArtIbex/MidiToys/tree/main/src'; // Replace <repo-name> with the repository name
 
 export { baseUrl };
